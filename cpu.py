fb8a85547849496212e8449377b92b01b1e6254e
Vyraal1 <kevin.afable@gmail.com>
2020-03-27 11:22:35 -0400

    feat: planning

---
+++ cpu.py
@@ -0,0 +1,13 @@
+# CPU class
+# branchtable
+# pc
+# register
+# memory
+
+
+# LDI
+# PRN
+# CMP
+# JMP
+# JEQ
+# JNE
---
+++ load.py
@@ -0,0 +1,6 @@
+import sys
+
+# instantiate CPU
+# get filename from args
+# load program
+# run
